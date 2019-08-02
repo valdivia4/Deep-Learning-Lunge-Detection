afab1089cac13b25424319b4aaf51e350a920f03
willi <mcclow12@stanford.edu>
2019-08-01 20:05:58 -0700

    Better comments

--- preprocessing/data_config.py
+++ preprocessing/data_config.py
@@ -4,23 +4,29 @@ class config():
 
     def __init__(self):
 
+        
         self.num_train_sets = 10 
-        self.window_s = 4
-        self.padded_window_s = 20
+        self.window_s = 4 #number of seconds for inner window, e.g.
+                          #window_s = 4 means a window is labeled 1 if
+                          #there is a lunge within 2 seconds of the middle
+        self.padded_window_s = 20 #total window seconds
 
-        ## near_pos_multiple is the multiple of near positive training examples to keep relative to positive training examples
-        ## random_neg_multiple is the multiple of randomly selected negative examples
-
-        self.near_pos_multiple = 0.2 ##make sure fs*multiple is an integer, usually fs=10
+        #near_pos_multiple is the multiple of near positive training examples
+        #to keep relative to positive training examples
+        #random_neg_multiple is the multiple of randomly selected negative 
+        #examples
+        self.near_pos_multiple = 0.2 
         self.rand_neg_multiple = 19.8
            
+        # Correction window settings
+        self.num_correction_windows_per_label = 10
+        self.correction_window_s = 16 #number of seconds in correction window
+        self.max_exp_perturbation = 5 #maximum expected mistake in labeling
+                                      #in seconds
+
+        #training set info
         self.num_features = 5
         self.fs = 10
-
-        # Correction windows
-        self.num_correction_windows_per_label = 10
-        self.correction_window_s = 16
-        self.max_exp_perturbation = 5
 
         self.num_files = 6
         self.train_files = [0,1,2,4]
