a7b1c965133cdee54c964d0a61f526fd81765115
willi <mcclow12@stanford.edu>
2019-08-14 20:42:04 -0700

    focal loss

--- train/model_configs.py
+++ train/model_configs.py
@@ -120,3 +120,18 @@ def get_weighted_bce(pos_weight):
                 pos_weight,
             )
     return weighted_bce
+
+def asym_binary_focal_loss(gamma0=1., gamma1=0., alpha=.25):
+    def asym_binary_focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+
+        epsilon = K.epsilon()
+        # clip to prevent NaN's and Inf's
+        pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
+        pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
+
+        return K.mean(-alpha * K.pow(1. - pt_1, gamma1) * K.log(pt_1) \
+                -(1 - alpha) * K.pow(pt_0, gamma0) * K.log(1. - pt_0))
+    return asym_binary_focal_loss_fixed
+
