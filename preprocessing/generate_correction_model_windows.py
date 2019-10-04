6a7e7ef16e97b1041c634ca145950d729ec04d96
willi <mcclow12@stanford.edu>
2019-10-04 13:07:03 -0500

    softmax correction model

--- preprocessing/generate_correction_model_windows.py
+++ preprocessing/generate_correction_model_windows.py
@@ -9,14 +9,19 @@ from data_config import config as data_config
 
 X_train = []
 Y_train_regression = []
+Y_train_class = []
 X_val = []
 Y_val_regression = []
+Y_val_class = []
 X_test = []
 Y_test_regression = []
+Y_test_class = []
 
 config = data_config()
 samples_per_window = config.correction_window_s * config.fs
 
+num_bins = int(2*config.fs*config.max_exp_perturbation)
+bins = np.linspace(-1, 1, num=num_bins)
 for i in range(config.num_files):
     X = np.load('./numpy_data/inputs/inputs_'+ str(i)+'.npy')
     Y = np.load('./numpy_data/labels/labels_'+ str(i)+'.npy')
@@ -31,6 +36,9 @@ for i in range(config.num_files):
             delta = round(delta*config.fs)
             window_center = index - delta
             scaled_delta = delta/(config.max_exp_perturbation*config.fs)
+            digit_delta = np.digitize(scaled_delta, bins, right=True)
+            class_delta = np.zeros((1, num_bins))
+            class_delta[0, digit_delta] = 1
             x = X[window_center-int(samples_per_window/2):window_center+int(samples_per_window/2),:]
     #         print(x.shape)
             if x.shape != (samples_per_window, config.num_features):
@@ -38,25 +46,31 @@ for i in range(config.num_files):
             if i in config.train_files:
                 X_train.append(x)
                 Y_train_regression.append(scaled_delta)
+                Y_train_class.append(class_delta)
             elif i in config.val_files:
                 X_val.append(x)
                 Y_val_regression.append(scaled_delta)
+                Y_val_class.append(class_delta)
             elif i in config.test_files:
                 X_test.append(x)
                 Y_test_regression.append(scaled_delta)
+                Y_test_class.append(class_delta)
                 
 X_train = np.stack(X_train)
 Y_train_regression = np.reshape(np.stack(Y_train_regression), (len(Y_train_regression),1))
+Y_train_class = np.reshape(np.stack(Y_train_class), (len(Y_train_class), num_bins))
 
 X_val = np.stack(X_val)
 Y_val_regression = np.reshape(np.stack(Y_val_regression), (len(Y_val_regression),1))
+Y_val_class = np.reshape(np.stack(Y_val_class), (len(Y_val_class), num_bins))
 
 X_test = np.stack(X_test)
 Y_test_regression = np.reshape(np.stack(Y_test_regression), (len(Y_test_regression),1))
+Y_test_class = np.reshape(np.stack(Y_test_class), (len(Y_test_class), num_bins))
 
-X_train, Y_train_regression = shuffle(X_train, Y_train_regression)
-X_val, Y_val_regression = shuffle(X_val, Y_val_regression)
-X_test, Y_test_regression = shuffle(X_test, Y_test_regression)
+X_train, Y_train_regression, Y_train_class = shuffle(X_train, 
+                                                Y_train_regression,
+                                                Y_train_class)
 
 folder = "../training_windows/correction_model_windows/"
 if not os.path.exists(folder):
@@ -64,8 +78,11 @@ if not os.path.exists(folder):
 
 np.save(folder + "X_train" , X_train)
 np.save(folder + "Y_train_regression", Y_train_regression)
+np.save(folder + "Y_train_class", Y_train_class)
 np.save(folder + "X_val" , X_val)
 np.save(folder + "Y_val_regression", Y_val_regression)
+np.save(folder + "Y_val_class", Y_val_class)
 np.save(folder + "X_test" , X_test)
 np.save(folder + "Y_test_regression", Y_test_regression)
+np.save(folder + "Y_test_class", Y_test_class)
 
---
+++ train/correction_model_class.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras import backend as K
+
+sys.path.append('../preprocessing/')
+from data_config import config as data_config
+
+config = data_config()
+
+X_train = np.load('../training_windows/correction_model_windows/X_train.npy')
+Y_train = np.load('../training_windows/correction_model_windows/Y_train_class.npy')
+X_val = np.load('../training_windows/correction_model_windows/X_val.npy')
+Y_val = np.load('../training_windows/correction_model_windows/Y_val_class.npy')
+
+print (X_train.shape)
+
+m_train, w, f = X_train.shape
+m_val, __, __  = X_val.shape
+X_train_f = np.reshape(X_train, (m_train, w*f))
+X_val_f = np.reshape(X_val, (m_val, w*f))
+
+input_dim = w*f
+num_bins = 2*config.fs*config.max_exp_perturbation
+
+model = Sequential([
+    Dense(32, input_dim=input_dim),
+    Activation('relu'),
+    BatchNormalization(),
+    Dense(20),
+    BatchNormalization(),
+    Activation('relu'),
+    Dense(5),
+    BatchNormalization(),
+    Activation('relu'),
+    Dense(num_bins),
+    Activation('softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['acc'])
+
+model.fit(X_train_f, Y_train, epochs=100, validation_data=(X_val_f,Y_val),batch_size=32)
+
+folder = '../models/correction_models/'
+if not os.path.exists(folder):
+    os.makedirs(folder)
+model.save(folder + 'correction_model.h5')
