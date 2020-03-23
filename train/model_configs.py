387a833948fac9a22c9f8edad0820c4607d4b4d9
mcclow12 <mcclow12@stanford.edu>
2020-03-23 01:26:56 -0500

    config for new model

--- train/model_configs.py
+++ train/model_configs.py
@@ -9,7 +9,7 @@ class feed_forward_config():
         # model and training config
         self.flattened_input = True #True for feed_forward
         self.near_pos_multiple = 0.2
-        self.rand_neg_multiple = 19.8 
+        self.rand_neg_multiple = 2.8
         self.num_train_sets = 10
             #these 3 lines determine the training set in case multiple
             #training sets have been generated.
@@ -18,7 +18,7 @@ class feed_forward_config():
         self.model_name = 'feed_forward'
 
         # hyper params
-        self.chaining_dists = [i for i in range(1,11)]
+        self.chaining_dists = [i for i in range(3,11)]
         self.thresholds = np.linspace(0.5, 0.9, 10)
             #used for consolidation of training labels
         self.tolerance_seconds = 5
@@ -29,7 +29,7 @@ class feed_forward_config():
         self.batch_size = 128
         self.optimizer = 'adam'
         self.learning_rate = 1e-3
-        self.hidden_layers = [32,20]
+        self.hidden_layers = []
             #number of nodes in each hidden layer 
         self.l2_reg = 0.0 
             #l2 regularization parameter
@@ -38,7 +38,7 @@ class feed_forward_config():
         self.activation = 'relu'
             #which nonlinear activation to use
         self.output_activation = 'sigmoid'
-        self.pos_weight = 1 #*
+        self.pos_weight = 2 #*
             #weight of positive class for weighted binary cross entropy
             #(pos_weight=1 corresponds to normal binary cross entropy)
         self.loss = get_weighted_bce(self.pos_weight)
@@ -50,21 +50,21 @@ class resnet_config():
 
         # model and training config
         self.flattened_input = False #False for resnet
-        self.near_pos_multiple = 0.2 
-        self.rand_neg_multiple = 19.8 
+        self.near_pos_multiple = 0.2
+        self.rand_neg_multiple = 2.8
         self.num_train_sets = 10 
         self.hyper_search = False 
         self.model_name = 'resnet'
 
         # hyper params
-        self.chaining_dists = [i for i in range(1,11)] 
-        self.thresholds = np.linspace(0.5, 0.9, 10)
+        self.chaining_dists = [i for i in range(3,11)] 
+        self.thresholds = np.linspace(0.5, 0.9, 4)
         self.tolerance_seconds = 5 
         self.n_iterations = 20
         self.batch_size = 128 
         self.optimizer = 'adam'
         self.learning_rate = 1e-3
-        self.pos_weight = 1 
+        self.pos_weight = 2 
         self.loss = get_weighted_bce(self.pos_weight)
         self.metrics=['accuracy']
 
@@ -74,33 +74,59 @@ class feed_forward_search_config():
         # model and training config
         self.flattened_input = True
         self.near_pos_multiple = 0.2
-        self.rand_neg_multiple = 19.8 
-        self.num_train_sets = 10 
+        self.rand_neg_multiple = 2.8 
+        self.num_train_sets = 40 
         self.hyper_search = True
         self.model_name = 'feed_forward'
 
         # hyper params
-        self.chaining_dists = [i for i in range(1,11)]
+        self.chaining_dists = [i for i in range(3,11)]
         self.thresholds = np.linspace(0.5, 0.9, 10)
         self.tolerance_seconds = 5
-        self.n_iterations = 20
+        self.n_iterations = 1
         self.batch_size = 128
         self.optimizer = 'adam'
         exp = -(2*np.random.random()+2)
         self.learning_rate = 10**exp
         h_layers = random.sample([[32, 20], [64, 32, 20], [128, 64, 32, 20]], 1)[0]
-        self.hidden_layers = h_layers
+        self.hidden_layers = [64, 32, 20]
         reg = random.sample([0., 1e-4, 1e-2, 1.], 1)[0]
         self.l2_reg = reg
         self.batch_norm = True
         self.activation = 'relu'
         self.output_activation = 'sigmoid'
         # self.loss = 'binary_crossentropy'
-        weight = random.sample([8, 18, 28], 1)[0]
-        self.pos_weight = weight
+        #weight = random.sample([], 1)[0]
+        exp = 0.26 + 0.74*np.random.random()
+        self.pos_weight = 2**exp
         self.loss = get_weighted_bce(self.pos_weight)
         self.metrics=['accuracy']
 
+class cnn_rnn_search_config():
+    def __init__(self):
+        #See ff_config comments for most variable descriptions
+
+        # model and training config
+        self.flattened_input = False #False for resnet
+        self.near_pos_multiple = 0.2
+        self.rand_neg_multiple = 2.8
+        self.num_train_sets = 40 
+        self.hyper_search = True 
+        self.model_name = 'resnet'
+
+        # hyper params
+        self.chaining_dists = [i for i in range(3,11)] 
+        self.thresholds = np.linspace(0.5, 0.9, 10)
+        self.tolerance_seconds = 5 
+        self.n_iterations = 1
+        self.batch_size = 128 
+        self.optimizer = 'adam'
+        exp = -(0.6*np.random.random()+2.7)
+        self.learning_rate = 10**exp
+        exp = 0.26 + 0.74*np.random.random()
+        self.pos_weight = 2**exp
+        self.loss = get_weighted_bce(self.pos_weight)
+        self.metrics=['accuracy']
 
 def get_config(name):
     if name =='feed_forward':
@@ -109,6 +135,8 @@ def get_config(name):
         return resnet_config()
     elif name == 'feed_forward_search':
         return feed_forward_search_config()
+    elif name == 'cnn_rnn_search':
+        return cnn_rnn_search_config()
 
 def get_weighted_bce(pos_weight):
     def weighted_bce(y_true,y_pred):
